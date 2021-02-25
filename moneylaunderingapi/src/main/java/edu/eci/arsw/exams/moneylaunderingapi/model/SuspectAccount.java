2595f77755901bc0810400fe41b67e48725fb58d
Nikolas2001-13 <nikolas.bernal@mail.escuelaing.edu.co>
2021-02-24 22:31:13 -0500

    punto 3

--- moneylaunderingapi/src/main/java/edu/eci/arsw/exams/moneylaunderingapi/model/SuspectAccount.java
+++ moneylaunderingapi/src/main/java/edu/eci/arsw/exams/moneylaunderingapi/model/SuspectAccount.java
@@ -1,6 +1,29 @@
 package edu.eci.arsw.exams.moneylaunderingapi.model;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class SuspectAccount {
     public String accountId;
     public int amountOfSmallTransactions;
+
+    public suspectAccount(String accountId, int amountOfSmallTransactions){
+        this.accountId = accountId;
+        this.amountOfSmallTransactions = new AtomicInteger(amountOfSmallTransactions);
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public int getAmountOfSmallTransactions() {
+        return amountOfSmallTransactions.get();
+    }
+
+    public void setAmountOfSmallTransactions(int amountOfSmallTransactions) {
+        this.amountOfSmallTransactions.getAndAdd(amountOfSmallTransactions);
+    }
 }
--- moneylaunderingapi/src/main/java/edu/eci/arsw/exams/moneylaunderingapi/service/MoneyLaunderingServiceStub.java
+++ moneylaunderingapi/src/main/java/edu/eci/arsw/exams/moneylaunderingapi/service/MoneyLaunderingServiceStub.java
@@ -1,24 +1,50 @@
 package edu.eci.arsw.exams.moneylaunderingapi.service;
 
 import edu.eci.arsw.exams.moneylaunderingapi.model.SuspectAccount;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
+@Service
 public class MoneyLaunderingServiceStub implements MoneyLaunderingService {
+    private List<SuspectAccount> suspectAccounts = new CopyOnWriteArrayList<>();
+
+    public MoneyLaunderingServiceStub() {
+        suspectAccounts.add(new SuspectAccount("1234", 500));
+        suspectAccounts.add(new SuspectAccount("5678", 200));
+        suspectAccounts.add(new SuspectAccount("9101", 1000));
+    }
+
     @Override
     public void updateAccountStatus(SuspectAccount suspectAccount) {
-        //TODO
+        this.getAccountStatus(suspectAccount.getAccountId()).setAmountOfSmallTransactions(suspectAccount.getAmountOfSmallTransactions());
     }
 
     @Override
     public SuspectAccount getAccountStatus(String accountId) {
-        //TODO
+        for (SuspectAccount suspectAccount:suspectAccounts) {
+            if (suspectAccount.getAccountId().equals(accountId)) {
+                return suspectAccount;
+            }
+        }
+
         return null;
     }
 
     @Override
     public List<SuspectAccount> getSuspectAccounts() {
-        //TODO
-        return null;
+        return suspectAccounts;
+    }
+
+    @Override
+    public synchronized void addSuspectAccounts(SuspectAccount account) {
+        for (SuspectAccount suspectAccount:suspectAccounts) {
+            if (suspectAccount.getAccountId().equals(account.getAccountId())) {
+                System.out.println("This Account Already Exist");
+            }
+        }
+
+        suspectAccounts.add(account);
     }
 }
